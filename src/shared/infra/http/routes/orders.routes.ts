3a087919b226e88b92b4baf8bb49f3f714a517c9
LucasEmanuel9611 <lucas190717@gmail.com>
2025-02-25 21:12:14 -0300

    feat: list orders, list by user, delete

--- src/shared/infra/http/routes/orders.routes.ts
+++ src/shared/infra/http/routes/orders.routes.ts
@@ -1,4 +1,7 @@
 import { CreateOrderController } from "@modules/orders/useCases/createOrder/CreateOrderController";
+import { DeleteOrderController } from "@modules/orders/useCases/deleteOrder/DeleteOrderController";
+import { ListOrdersController } from "@modules/orders/useCases/listOrders/listOrdersController";
+import { ListOrdersByUserController } from "@modules/orders/useCases/listOrdersByUser/listOrdersByUserController";
 import { Router } from "express";
 
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
@@ -6,5 +9,15 @@ import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 export const orderRoutes = Router();
 
 const createOrderController = new CreateOrderController();
+const deleteOrderController = new DeleteOrderController();
+const listOrdersController = new ListOrdersController();
+const listOrdersByUserController = new ListOrdersByUserController();
 
 orderRoutes.post("/", ensureAuthenticated, createOrderController.handle);
+orderRoutes.delete("/", ensureAuthenticated, deleteOrderController.handle);
+orderRoutes.get("/list/all", ensureAuthenticated, listOrdersController.handle);
+orderRoutes.get(
+  "/user/list",
+  ensureAuthenticated,
+  listOrdersByUserController.handle
+);
