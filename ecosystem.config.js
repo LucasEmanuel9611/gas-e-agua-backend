77bf3de43cd68697ac3ec4bcd9ef89706c40d549
LucasEmanuel9611 <lucas190717@gmail.com>
2025-04-16 19:04:33 -0300

    chore: ecossytem config  for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: "meu-app-dev",
+      script: "yarn",
+      args: "dev",
+      watch: true, // reinicia ao detectar alterações nos arquivos
+      max_restarts: 10, // máximo de tentativas de restart
+      restart_delay: 1000, // espera 1 segundo entre os restarts
+      autorestart: true, // garante que ele sempre tente reiniciar
+      env: {
+        NODE_ENV: "development",
+      },
+    },
+  ],
+};
