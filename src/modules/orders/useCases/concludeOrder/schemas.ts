f89e3b4169b9bf579f6e3d8fab8cd92b95258acd
LucasEmanuel9611 <lucas190717@gmail.com>
2025-05-29 16:36:41 -0300

    feat: conclude order use case

---
+++ src/modules/orders/useCases/concludeOrder/schemas.ts
@@ -0,0 +1,19 @@
+import { OrderStatusProps } from "@modules/orders/types";
+import { z } from "zod";
+
+export const concludeOrderSchema = z.object({
+  order_id: z.string({
+    required_error: "O ID do pedido é obrigatório",
+  }),
+  status: z.union(
+    [z.literal("FINALIZADO"), z.literal("CANCELADO"), z.literal("PENDENTE")],
+    {
+      errorMap: (issue, ctx) => {
+        if (issue.code === "invalid_type") {
+          return { message: "O status é obrigatório" };
+        }
+        return { message: "Status inválido" };
+      },
+    }
+  ) as z.ZodType<OrderStatusProps>,
+});
